2523b62ae6ba0b2e6a7fa20adf1d7a6d95842973
Briant Gale <briantgale@gmail.com>
2022-12-08 17:12:54 -0700

    Autoload my day classes and create the class for me

--- lib/advent.rb
+++ lib/advent.rb
@@ -3,14 +3,60 @@
 require 'pry'
 require_relative "advent/version"
 require_relative "advent/helper"
-require_relative "advent/day_1"
-require_relative "advent/day_2_base"
-require_relative "advent/day_2"
-require_relative "advent/day_2_b"
-require_relative "advent/day_3"
-require_relative "advent/day_4"
+
+# Dynamicly load my day classes
+dir = File.join(Dir.pwd, "lib", "advent")
+Dir.entries(dir).each do |entry|
+  next if !File.file?(File.join(dir, entry))
+  next if !entry.start_with?("day")
+
+  s = "advent/#{entry.gsub('.rb', '')}"
+  require_relative s
+end
 
 module Advent
   class Error < StandardError; end
-  # Your code goes here...
+
+  class << self
+    def new_day(day)
+      if File.exists?(File.join(lib_advent_path, file_name_for_day(day)))
+        puts "File already exists"
+        return
+      end
+      
+      File.open(File.join(lib_advent_path, file_name_for_day(day)), "w+") do |f|
+        f.write klass_contents(day)
+      end
+
+      puts "Created class for day #{day}"
+    end
+
+    private
+
+    def file_name_for_day(day)
+      "day_#{day}.rb"
+    end
+
+    def lib_advent_path
+      File.join(app_path, "lib", "advent")
+    end
+
+    def app_path
+      File.join(Dir.pwd)
+    end
+
+    def klass_contents(day)
+      <<-EOS
+class Advent::Day#{day}
+  include Advent::Helper
+
+  RUN = %i(run)
+
+  def run
+    "run"
+  end
+end
+EOS
+    end
+  end
 end
